7b59f8fca8d381e127461be483261fdc60312c86
rafacarrenho <rafael.carrenho@hotmail.com>
2021-05-06 10:19:36 -0300

    first commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+export function Counter() {
+  const [counter, setCounter] = useState(9);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <div>
+      <h2>{counter}</h2>
+      <button type="button" onClick={increment}>
+        {" "}
+        Incremento + 1
+      </button>
+    </div>
+  );
+}
